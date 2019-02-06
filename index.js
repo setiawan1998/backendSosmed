3b5738d1bf4fbd37f310bc48aa463bf5ca1912c5
Setiawan Restu Aji 84 <setiawannuha123@gmail.com>
2019-02-07 00:40:00 +0700

    cors

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const routes = require('./start/routes');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
